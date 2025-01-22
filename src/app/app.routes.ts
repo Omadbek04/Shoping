7bd6b79e3e52bcd4e527c9f74e68a77f2bfddf28
omadek <abdullayevomadbek709@gmail.com>
2025-01-22 15:16:59 +0500

    route ozgartirildi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
-import{MenuComponent} from "./menu/menu.component"
+import { MenuComponent } from './menu/menu.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'menu', // Agar path bosh bo'lsa, "menu" ga yo'naltiradi
+    pathMatch: 'full', // "full" bilan to'liq mos bo'lishini tekshiradi
+  },
   {
     path: 'cart',
     component: CartComponent,
   },
   {
     path: 'menu',
-    component:MenuComponent ,
+    component: MenuComponent,
   },
 ];
